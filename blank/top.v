499aa00fc924e90eec7de5246b53bd1faa8ebf5b
pdmart <pdmart@gmail.com>
2016-05-21 13:08:27 -0700

    added blank project folder

---
+++ blank/top.v
@@ -0,0 +1,15 @@
+// Blink an LED provided an input clock
+/* module */
+module top (hwclk, led1, led2, led3, led4, led5, led6, led7, led8 );
+    /* I/O */
+    input hwclk;
+    output led1;
+    output led2;
+    output led3;
+    output led4;
+    output led5;
+    output led6;
+    output led7;
+    output led8;
+
+endmodule
