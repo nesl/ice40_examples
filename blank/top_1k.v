69fe77f1ae43939b8f09a2a87a606f0066e90341
Steffen Moeller <moeller@debian.org>
2016-08-07 21:58:45 +0200

    blank compatible with HX1K

---
+++ blank/top_1k.v
@@ -0,0 +1,12 @@
+// Blink an LED provided an input clock
+/* module */
+module top (hwclk, led1, led2, led3, led4, led5);
+    /* I/O */
+    input hwclk;
+    output led1;
+    output led2;
+    output led3;
+    output led4;
+    output led5;
+
+endmodule
