985e4fa0c5335e9983f149a1b1350cd3cbd488a4
Sean Anderson <seanga2@gmail.com>
2019-01-27 16:02:18 -0500

    Add common.mk
    Forgot to do this before pushing (whoops)

---
+++ common.mk
@@ -0,0 +1,31 @@
+# Really ugly, but make can't decrement so we get this...
+getincluder = $(word $(words $(wordlist 2,$(words $1),$1)),$1)
+DIR := $(dir $(call getincluder,$(MAKEFILE_LIST)))
+
+ifndef BUILD
+.PHONY: all burn clean
+
+all:
+	$(MAKE) -C .. $(MODULE)
+
+burn:
+	$(MAKE) -C .. burn-$(MODULE)
+
+clean:
+	$(MAKE) -C .. $@
+endif
+
+VPATH += $(DIR)
+
+M_VSRC := $(addprefix $(DIR),$(M_VSRC))
+M_SRC := $(M_VSRC) $(addprefix $(DIR),$(M_SRC))
+SRC += $(M_SRC)
+
+.PHONY: $(MODULE)
+$(MODULE): $(BUILD)/$(MODULE).bin
+
+$(M_SRC): $(DIR)Makefile $(lastword $(MAKEFILE_LIST))
+
+$(BUILD)/$(MODULE).blif: $(M_VSRC)
+
+all: $(MODULE)
